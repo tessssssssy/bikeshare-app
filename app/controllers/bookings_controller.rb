271a687a7ae536429d6aab1861464cd23bfdeedb
tessssssssy <tessfabry@gmail.com>
2020-05-21 17:29:28 +1000

    clean up bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -21,16 +21,16 @@ class BookingsController < ApplicationController
   end
 
   # create a booking
-  def create
-    @booking = Booking.new
-    # for some reason itwould not let me create a booking using booking_params and also add the listing_id, so I had to manually add each attribute
-    @booking.user_id = current_user.id
-    @booking.listing_id = params[:listing_id]
-    @booking.start_date = params[:booking][:start_date]
-    @booking.end_date = params[:booking][:end_date]
-    @booking.start_time = params[:booking][:start_time]
-    @booking.end_time = params[:booking][:end_time]
-
+  def create    
+    @booking = Booking.new do |booking|
+      booking.user_id = current_user.id
+      booking.listing_id = params[:listing_id]
+      booking.start_date = params[:booking][:start_date]
+      booking.end_date = params[:booking][:end_date]
+      booking.start_time = params[:booking][:start_time]
+      booking.end_time = params[:booking][:end_time]
+    end
+    # for some reason it would not let me create a booking using booking_params and also add the listing_id, so I had to manually add each attribute
     # automatically sets confirmed to true if a listing is booked by its own user
     # if it's a different user they have to pay a deposit before it is confirmed
     @listing = Listing.find(params[:listing_id])
@@ -38,25 +38,25 @@ class BookingsController < ApplicationController
     # lots of conditionals for invaild date inputs  or unavailable dates and times with flash messages
     if @booking.start_date.nil? || @booking.end_date.nil?
       flash[:notice] = 'Please select a start date and an end date'
-      redirect_to "/listings/#{@listing.id}/bookings/new"
+      redirect_to new_listing_booking_path
     elsif date_to_integer(@booking.start_date) > date_to_integer(@booking.end_date)
       flash[:notice] = 'End date cannot be before start date'
-      redirect_to "/listings/#{@listing.id}/bookings/new"
+      redirect_to new_listing_booking_path
     elsif @booking.start_date == @booking.end_date && @booking.start_time >= @booking.end_time
       flash[:notice] = 'Finish time must be after start time'
-      redirect_to "/listings/#{@listing.id}/bookings/new"
+      redirect_to new_listing_booking_path
     elsif !@listing.check_availability(@booking.start_date, @booking.end_date)
       flash[:notice] = 'Booking Dates Unavailable'
-      redirect_to "/listings/#{@listing.id}/bookings/new"
+      redirect_to new_listing_booking_path
     elsif !@listing.time_available?(@booking.start_time, @booking.start_date) || !@listing.time_available?(@booking.end_time, @booking.end_date)
       flash[:notice] = "Available Times: #{@listing.get_available_times(@booking.start_date)}, #{@listing.get_available_times(@booking.end_date)}"
-      redirect_to "/listings/#{@listing.id}/bookings/new"
+      redirect_to new_listing_booking_path
     else
       if @booking.save
         if current_user.id == @listing.user_id
           redirect_to listings_path
         else
-          redirect_to "/listings/#{@booking.listing_id}/bookings/#{@booking.id}"
+          redirect_to "/listings/#{@listing.id}/bookings/#{@booking.id}"
           end
       else
         render :new
